792f6795e26d23a23047f5027245fa8434ed2ed9
Hristo <h.georgiew93@gmail.com>
2021-02-24 09:51:20 +0200

    Add route Products

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,10 @@
 const expresss = require('express');
 const app = expresss();
 
+app.use(expresss.json());
+
+//import all routes
+const products = require('./routes/product');
+
+app.use('/api/v1', products);
 module.exports = app;
