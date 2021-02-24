9079722428ffcb45337dd5bb47c7bac787355b99
Hristo <h.georgiew93@gmail.com>
2021-02-24 12:21:04 +0200

    Update product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -9,11 +9,90 @@ const productSchema = new mongoose.Schema({
     },
 
     price: {
-        type: String,
-        required: [true, 'Please enter product name'],
-        trim: true,
-        maxLenght: [100, 'Product name cannot exceed 100 characters']
+        type: Number,
+        required: [true, 'Please enter product price'],
+        maxLenght: [5, 'Product name cannot exceed 5 characters'],
+        default: 0.0
     },
+
+    description: {
+        type: String,
+        required: [true, 'Please enter product description'],
+    },
+
+    ratings: {
+        type: Number,
+        default: 0
+    },
+
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            public_id: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+
+    category: {
+        type: String,
+        required: [true, 'Please select category for this product'],
+        enum: {
+            values: [
+                'Electronics',
+                'Cameras',
+                'Laptop',
+                'Accessories',
+                'Food',
+                'Books',
+                'Clothes/Shoes',
+                'Beauty/Health',
+                'Sports',
+                'Outdoor',
+                'Home'
+            ],
+            message: 'Please select correct category for product'
+        }
+    },
+
+    seller: {
+        type: String,
+        required: [true, 'Please enter product seller']
+    },
+    stock: {
+        type: Number,
+        required: [true, 'Please enter product stock'],
+        maxLenght: [5, 'Product name cannot exceed 5 characters'],
+        default: 0
+    },
+    numOfReviews: {
+        type: Number,
+        default: 0
+    },
+    reviews: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            rating: {
+                type: Number,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = mongoose.model('product', productSchema);
